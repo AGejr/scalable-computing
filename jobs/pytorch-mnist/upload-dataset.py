c2c2fa6ad502bd21cf4f52e4c06f4a9ad33d9252
Albert Gejr <albni20@student.sdu.dk>
2024-10-02 14:12:46 +0200

    GCS for workloads (#1)
    * Add gcs bucket to infra
    * Add service accounts to infra in order to grant access to GCS from a workload
    * Add container registry repo to infra
    * Add python venv to gitignore
    * Add hello-gcs workload that tests write access to GCS bucket
    * Update custom pytorch mnist job to retrieve dataset from bucket

---
+++ jobs/pytorch-mnist/upload-dataset.py
@@ -0,0 +1,30 @@
+import torch
+from torchvision import datasets, transforms
+import os
+
+# Define a transform to normalize the data
+transform = transforms.Compose([transforms.ToTensor()])
+
+# Download and load the training data
+trainset = datasets.FashionMNIST('./data/F_MNIST_data/', download=True, train=True, transform=transform)
+
+# Download and load the test data
+testset = datasets.FashionMNIST('./data/F_MNIST_data/', download=True, train=False, transform=transform)
+
+print("FashionMNIST dataset downloaded and saved successfully.")
+# Define the directory to save the dataset
+save_dir = './data'
+os.makedirs(save_dir, exist_ok=True)
+
+# Save the training data
+torch.save(trainset, os.path.join(save_dir, 'trainset.pth'))
+
+# Save the test data
+torch.save(testset, os.path.join(save_dir, 'testset.pth'))
+
+print(f"Training and test datasets saved to {save_dir}")
+
+os.system('gcloud storage cp ./data/trainset.pth gs://ml-model-bucket-123456/trainset.pth')
+os.system('gcloud storage cp ./data/testset.pth gs://ml-model-bucket-123456/testset.pth')
+
+os.system('rm -rf ./data')
