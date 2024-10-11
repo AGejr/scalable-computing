9556a5c136caca32695beb1bd3cb6c9c8b6a6b8d
AGejr <albni20@student.sdu.dk>
2024-10-11 08:29:20 +0200

    Added initial yolo job

--- jobs/yolo/yolo.py
+++ jobs/yolo/yolo.py
@@ -2,11 +2,10 @@ from __future__ import print_function
 
 from models.yolo import Model
 from utils.general import (
-
     check_amp
 )
 
-model = Model("C:\\Users\\mglad\\Desktop\\anaconda\\yolov5-master\\models\\yolov5n.yaml", ch=3, nc=1)  # create
+model = Model("./yolov5n.yaml", ch=3, nc=1)  # create
 amp = check_amp(model)  # check AMP
 print("loaded")
 
